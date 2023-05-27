81e1849fc7a343e0d699785734dd06fdf887d7ee
call_me_zer <76487961+zer69@users.noreply.github.com>
2023-05-28 02:33:46 +0300

    new layout slight tweak

--- Assets/Scripts/Stats/Graph.cs
+++ Assets/Scripts/Stats/Graph.cs
@@ -9,6 +9,7 @@ public class Graph : MonoBehaviour
     [SerializeField] private Sprite dotSprite;
     private RectTransform graphContainer;
     public float dotSize;
+    private float newDotSize;
     
     public List<Color> colorList;
 
@@ -17,6 +18,7 @@ public class Graph : MonoBehaviour
         graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
         colorList[0] = new Color(colorList[0].r, colorList[0].g, colorList[0].b, 1f);
         colorList[1] = new Color(colorList[1].r, colorList[1].g, colorList[1].b, 1f);
+        newDotSize = dotSize;
     }
 
     private GameObject CreateDotVisual(Vector2 anchoredPosition, bool dnf)
@@ -26,7 +28,7 @@ public class Graph : MonoBehaviour
         gameObject.GetComponent<Image>().sprite = dotSprite;
         RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = anchoredPosition;
-        rectTransform.sizeDelta = new Vector2(dotSize, dotSize);
+        rectTransform.sizeDelta = new Vector2(newDotSize, newDotSize);
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
         if (dnf)
@@ -44,11 +46,11 @@ public class Graph : MonoBehaviour
         }
         if (loadout.recordList.Count == 0)
             return;
-
+        
         
 
         
-        float graphHeight = graphContainer.sizeDelta.y - dotSize;
+        float graphHeight = graphContainer.sizeDelta.y - newDotSize;
         float yMaximum = (float)loadout.MaxTime();
         float xSize = graphContainer.sizeDelta.x / (loadout.recordList.Count + 1);
 
@@ -56,7 +58,7 @@ public class Graph : MonoBehaviour
         for (int i = 0; i < loadout.recordList.Count; i++)
         {
             float xPosition = (i + 1) * xSize;
-            float yPosition = (loadout.ParseRecord(loadout.recordList[i]) / yMaximum) * graphHeight + dotSize/2;
+            float yPosition = graphHeight - ((loadout.ParseRecord(loadout.recordList[i]) / yMaximum) * graphHeight) + newDotSize / 2;
             GameObject dotVisualGameObject = CreateDotVisual(new Vector2(xPosition, yPosition), loadout.recordList[i].dnf);
             if (lastDotGameObject != null)
             {
@@ -81,7 +83,7 @@ public class Graph : MonoBehaviour
         float distance = Vector2.Distance(dotPositionA, dotPositionB);
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
-        rectTransform.sizeDelta = new Vector2(distance, dotSize/5f);
+        rectTransform.sizeDelta = new Vector2(distance, newDotSize / 5f);
         rectTransform.anchoredPosition = dotPositionA + dir * distance * 0.5f;
         rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
 
